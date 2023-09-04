36b83a1f2efdff0be81b651b0c639ca2c718d5a4
Mark Grebe <grebe@ucmo.edu>
2023-09-03 19:38:12 -0500

    Add directory constants

--- autograde.py
+++ autograde.py
@@ -1,24 +1,26 @@
 #!/Users/markg/.pyenv/shims/python
-
 from pathlib import Path
 import os
+
+ZIPDIR = "./zips/"
+FILEDIR = "./files/"
 
 # Unzip the Blackboard Assignment archive and unzip it into the zips directory
 os.system("unzip -d zips/ gradebook*.zip")
 
 # Rename all of the assignment zips, leaving just lastname700.zip
-zipdir = Path("./zips/")
+zipdir = Path(ZIPDIR)
 for file in zipdir.glob('*.zip'):
     new_name = file.name.split('_')[-1]
-    os.rename("zips/" + file.name, "zips/" + new_name)
+    os.rename(ZIPDIR + file.name, ZIPDIR + new_name)
 
 # For each of the assignment zip files...
 for file in zipdir.glob('*.zip'):
     # Unzip the assignment into Student dir inside the files directory
-    os.system("unzip -d files/" + file.stem + " zips/" + file.name)
+    os.system("unzip -d " + FILEDIR + file.stem + " " + ZIPDIR + file.name)
 
     # Compile every C file in the assignment directory
-    student_dir_str = "files/" + file.stem
-    student_dir = Path(student_dir_str)
-    for cfile in student_dir.glob('*.c'):
-        os.system("clang -o " + student_dir_str + "/" + cfile.stem + " " + student_dir_str + "/" + cfile.name)
+    student_dir = FILEDIR + file.stem
+    student_path = Path(student_dir)
+    for cfile in student_path.glob('*.c'):
+        os.system("clang -o " + student_dir + "/" + cfile.stem + " " + student_dir + "/" + cfile.name)
